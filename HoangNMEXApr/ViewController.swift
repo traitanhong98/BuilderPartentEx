730454ba5574a93feb6b455991327eb3f47ac74f
HoangNM <hoangnm@ttc-solutions.com>
2021-04-14 09:57:16 +0700

    Initial Commit

---
+++ HoangNMEXApr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoangNMEXApr
+//
+//  Created by Hoang on 4/14/21.
+//  Copyright © 2021 Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
